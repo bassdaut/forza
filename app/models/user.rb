30a93c8972d0fa6604b3de7c86d72a72fa6adfe6
gabrielbaseggioplum <abaseggio@plum.com.ar>
2025-02-09 23:47:05 -0300

    Add the Routine model with the corresponding associations to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
 
   enum :role, { trainer: 0, trainee: 1 }, default: :trainer
 
+  has_many :routines, foreign_key: "trainee_id"
+  has_many :managed_routines, class_name: "Routine", foreign_key: "trainer_id"
+
   validates :name, :surname, :role, presence: true
 end
