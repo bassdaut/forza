8704a30385d8ae110f52255aff6f1b62cae1609b
gabrielbaseggioplum <abaseggio@plum.com.ar>
2025-02-09 23:45:58 -0300

    Add migration for Routine

---
+++ db/migrate/20250210022008_create_routines.rb
@@ -0,0 +1,11 @@
+class CreateRoutines < ActiveRecord::Migration[8.0]
+  def change
+    create_table :routines, id: :uuid do |t|
+      t.string :name
+      t.references :trainer, null: false, type: :uuid, foreign_key: { to_table: :users }
+      t.references :trainee, null: false, type: :uuid, foreign_key: { to_table: :users }
+      
+      t.timestamps
+    end
+  end
+end
